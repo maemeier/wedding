c1dcbf73982e4df433f3da9f0583d466d5d675bf
mae meier <maemeier@live.com>
2019-12-04 23:05:34 +0100

    create routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Places from "./pages/Places";
 import SinglePlace from "./pages/SinglePlace";
@@ -7,10 +8,9 @@ import Error from "./pages/Error";
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Places />
-      <SinglePlace />
-      <Error />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/places" component={Places} />
+      <Route exact path="/single-place" component={SinglePlace} />
     </div>
   );
 }
