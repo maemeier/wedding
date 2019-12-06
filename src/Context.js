0c11a77c4f00878460bda96eb1a1a374df90bf8c
Mae Kangsadan <maemeier@live.com>
2019-12-06 12:51:22 +0100

    fix content compo

--- src/Context.js
+++ src/Context.js
@@ -6,7 +6,7 @@ class RoomProvider extends Component {
   state = {
     rooms: [],
     sortedRooms: [],
-    featuredRoom: [],
+    featuredRooms: [],
     loading: true
   };
 
@@ -14,7 +14,6 @@ class RoomProvider extends Component {
 
   componentDidMount() {
     let rooms = this.formatData(items);
-
     let featuredRooms = rooms.filter(room => room.featured === true);
     this.setState({
       rooms,
@@ -28,6 +27,7 @@ class RoomProvider extends Component {
     let tempItems = items.map(item => {
       let id = item.sys.id;
       let images = item.fields.images.map(image => image.fields.file.url);
+
       let room = { ...item.fields, images, id };
       return room;
     });
--- src/components/FeatureRooms.js
+++ src/components/FeatureRooms.js
@@ -8,12 +8,16 @@ export default class FeaturedRooms extends Component {
 
   render() {
     let { loading, featuredRooms: rooms } = this.context;
-
+    console.log(rooms);
+    rooms = rooms.map(room => {
+      return <Room key={room.id} room={room} />;
+    });
     return (
       <section className="featured-rooms">
         <Title title="featured rooms" />
+
         <div className="featured-rooms-center">
-          <Loading />
+          {loading ? <Loading /> : rooms}
         </div>
       </section>
     );
