3a7bc0811b5cf8186f852b6b7bf8b342c59bacc4
Mae Kangsadan <maemeier@live.com>
2019-12-06 11:03:52 +0100

    add room component

--- src/components/FeatureRooms.js
+++ src/components/FeatureRooms.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
+import Title from "./Title";
 import { RoomContext } from "../context";
+import Room from "./Room";
 import Loading from "./Loading";
-
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
+
   render() {
-    const { featuredRooms: rooms } = this.context;
-    console.table(rooms);
+    let { loading, featuredRooms: rooms } = this.context;
+
     return (
-      <div>
-        welcome to website
-        <Loading />
-      </div>
+      <section className="featured-rooms">
+        <Title title="featured rooms" />
+        <div className="featured-rooms-center">
+          <Loading />
+        </div>
+      </section>
     );
   }
 }
---
+++ src/components/Room.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Room = () => {
+  return <div>This is room component</div>;
+};
+
+export default Room;
